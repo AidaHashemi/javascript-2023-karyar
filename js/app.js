76ac349bb66686a704e2ffef5d535e4381ba5e7c
Aida Hashemi <aidahashemiam@gmail.com>
2023-11-29 19:21:01 -0800

    last commit

--- js/app.js
+++ js/app.js
@@ -8,7 +8,8 @@ const editCardModal = document.getElementById("modal-edit-card");
 const editTaskInput = document.getElementById("edit-task-input");
 const addTaskInput = document.getElementById("add-task-input");
 const addCardModal = document.getElementById("modal-add-card");
-const closeWindow = document.querySelector(".close-window");
+const closeEditWindow = document.querySelector(".close-edit-window");
+const closeAddWindow = document.querySelector(".close-add-window");
 const allDoItems = document.getElementById("all-do-items");
 const allDoneItems = document.getElementById("all-done-items");
 // url
@@ -18,35 +19,46 @@ btnOrange.addEventListener("click", function () {
   cssStyles.addModal();
 });
 
-closeWindow.addEventListener("click", function () {
+closeEditWindow.addEventListener("click", function () {
+  cssStyles.closeModal();
+});
+closeAddWindow.addEventListener("click", function () {
   cssStyles.closeModal();
 });
 
-addForm.addEventListener("submit", (e) => {
+addForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   if (addTaskInput.value.trim()) {
     const data = {
       title: addTaskInput.value,
     };
-    creatItem(data);
+    await creatItem(data);
+    cssStyles.closeModal();
+    btnOrange.focus();
+    await getDoItems();
+    await getDoneItems();
   } else {
     alert("you should write something to add...");
   }
 });
 
-editForm.addEventListener("submit", (e) => {
+editForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   if (editTaskInput.value.trim()) {
     const data = {
       title: editTaskInput.value,
     };
-    editItem(data);
+    await editItem(data);
+    cssStyles.closeModal();
+    btnOrange.focus();
+    await getDoItems();
+    await getDoneItems();
   } else {
     alert("you should write something to edit...");
   }
 });
 
-allDoItems.addEventListener("click", (e) => {
+allDoItems.addEventListener("click", async (e) => {
   if (e.target.classList.contains("item-bar")) {
     const showIcons = e.target.parentElement.querySelector(".show-icons");
     showIcons.style.visibility = "visible";
@@ -57,25 +69,31 @@ allDoItems.addEventListener("click", (e) => {
     const id = e.target.parentElement.id;
     const confirmResponse = confirm("Do you want to delete this item?!");
     if (confirmResponse) {
-      deleteDoItem(id);
+      await deleteDoItem(id);
+      await getDoItems();
+      await getDoneItems();
     }
   }
   if (e.target.classList.contains("edit-icon")) {
     const id = e.target.parentElement.id;
     editForm.id = id;
     cssStyles.editDoItem();
-    getEditItem(id);
+    await getEditItem(id);
     editTaskInput.focus();
+    await getDoItems();
+    await getDoneItems();
   }
 
   if (e.target.classList.contains("check-icon")) {
     const id = e.target.parentElement.id;
-    getCheckedItem(id);
-    deleteDoItem(id);
+    await getCheckedItem(id);
+    await deleteDoItem(id);
+    await getDoItems();
+    await getDoneItems();
   }
 });
 
-allDoneItems.addEventListener("click", (e) => {
+allDoneItems.addEventListener("click", async (e) => {
   if (e.target.classList.contains("item-bar")) {
     const showIcons = e.target.parentElement.querySelector(".show-icons");
     showIcons.style.visibility = "visible";
@@ -85,35 +103,21 @@ allDoneItems.addEventListener("click", (e) => {
     const id = e.target.parentElement.id;
     const confirmResponse = confirm("Do you want to delete this item?!");
     if (confirmResponse) {
-      deleteDoneItem(id);
+      await deleteDoneItem(id);
+      await getDoItems();
+      await getDoneItems();
     }
   }
   if (e.target.classList.contains("undo-icon")) {
     const id = e.target.parentElement.id;
-    getUndoItem(id);
-    deleteDoneItem(id);
+    await getUndoItem(id);
+    await deleteDoneItem(id);
+    await getDoItems();
+    await getDoneItems();
   }
 });
 
-// declaring object variable
-const cssStyles = {
-  addModal: function () {
-    addCardModal.style.display = "flex";
-    btnOrange.style.display = "none";
-    addTaskInput.focus();
-  },
-  closeModal: function () {
-    editCardModal.style.display = "none";
-    addCardModal.style.display = "none";
-    btnOrange.style.display = "flex";
-  },
-  editDoItem: function () {
-    editCardModal.style.display = "flex";
-    addCardModal.style.display = "none";
-    btnOrange.style.display = "none";
-  },
-};
-//functions
+///// create do and done items
 const creatItem = async function (data) {
   const respone = await fetch(url + "/doItems", {
     method: "POST",
@@ -133,7 +137,7 @@ const createDoneItem = async function (data) {
     },
   });
 };
-
+///// delete do and done items
 const deleteDoItem = async function (id) {
   const respone = await fetch(url + `/doItems/${id}`, {
     method: "DELETE",
@@ -145,7 +149,7 @@ const deleteDoneItem = async function (id) {
   });
 };
 
-//  edit item to-do
+/////edit do item
 const getEditItem = async function (id) {
   const response = await fetch(url + `/doItems/${id}`);
   const item = await response.json();
@@ -160,56 +164,77 @@ const editItem = async function (data) {
     },
   });
 };
-// check item
+///// done item (to-do section)
 const getCheckedItem = async function (id) {
   const respone = await fetch(url + `/doItems/${id}`);
   const checkedItem = await respone.json();
   const data = {
     title: checkedItem.title,
   };
-  createDoneItem(data);
+  await createDoneItem(data);
 };
+/////undo items (done section)
 const getUndoItem = async function (id) {
   const respone = await fetch(url + `/doneItems/${id}`);
   const undoItem = await respone.json();
   const data = {
     title: undoItem.title,
   };
-  creatItem(data);
+  await creatItem(data);
 };
-
+///// get all items
 const getDoItems = async function () {
+  allDoItems.innerHTML = "";
   const respone = await fetch(url + "/doItems");
-  const items = await respone.json();
-  for (const item of items) {
+  const doItems = await respone.json();
+  for (const item of doItems) {
     allDoItems.innerHTML += `<li class="item" data-id="${item.id}">
     <span class="item-title">${item.title}</span>
     <i class="fas fa-bars item-bar"></i>
     <span class="show-icons" id="${item.id}">
-      <i class="far fa-minus-square delete-icon"></i>
-      <i class="far fa-edit edit-icon"></i>
-      <i class="far fa-check-square check-icon"></i>
+      <i class="far fa-minus-square delete-icon" title="DELETE"></i>
+      <i class="far fa-edit edit-icon" title="EDIT"></i>
+      <i class="far fa-check-square check-icon" title="DONE"></i>
     </span>
   </li>`;
   }
 };
 
 const getDoneItems = async function () {
+  allDoneItems.innerHTML = "";
   const respone = await fetch(url + "/doneItems");
-  const items = await respone.json();
-  for (const item of items) {
+  const doneItems = await respone.json();
+  for (const item of doneItems) {
     allDoneItems.innerHTML += `<li class="item" data-id="${item.id}">
     <span class="item-title">${item.title}</span>
     <i class="fas fa-bars item-bar"></i>
     <span class="show-icons" id="${item.id}">
-      <i class="far fa-minus-square delete-icon"></i>
-      <i class="fas fa-undo undo-icon"></i>
+      <i class="far fa-minus-square delete-icon" title="DELETE"></i>
+      <i class="fas fa-undo undo-icon" title="UNDO"></i>
     </span>
   </li>`;
   }
 };
 
-// ________________________________
+// cssStyles
+const cssStyles = {
+  addModal: function () {
+    addCardModal.style.display = "flex";
+    btnOrange.style.display = "none";
+    addTaskInput.focus();
+  },
+  closeModal: function () {
+    editCardModal.style.display = "none";
+    addCardModal.style.display = "none";
+    btnOrange.style.display = "flex";
+  },
+  editDoItem: function () {
+    editCardModal.style.display = "flex";
+    addCardModal.style.display = "none";
+    btnOrange.style.display = "none";
+  },
+};
+//////
 btnOrange.focus();
 getDoItems();
 getDoneItems();
